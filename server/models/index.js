d343cf2a731bcb741ffa5531ea9b977fd0ca788f
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-04 17:05:54 -0700

    Adding basic configuration, along with experimentation around server controllers, routes, and models

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+exports.products = require('./products.js');
+exports.reviews = require('./reviews.js');
+exports.questions = require('./questions.js');
+exports.cart = require('./cart.js');
+exports.answers = require('./answers.js');
---
+++ server/server.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+const router = require('./routes.js');
+
+const app = express();
+const port = 3000;
+
+// app.use(express.json()); // do we need this?
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// app.use(express.urlencoded({extended: false })); // will we need this?
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
