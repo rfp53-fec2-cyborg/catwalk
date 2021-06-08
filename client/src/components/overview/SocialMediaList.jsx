89234f5f860f8a8aa3ebf3757c721ced8bc960cd
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-07 19:01:16 -0700

    Work-in-progress: [Overview] product information section
    - Added Overview component renders a basic version of the product information section, with category, name, price (formatted), description
    - Added SocialMediaList and SocialMediaButton components for social media links below the product
    - Added description component to render the product slogan and description
    - Shared directory within components that includes StarRating component, which currently just renders the rating number, not the starts
    - Created an assets directory to hold svg images
    - The logic to compute rating lives in it's own module, and anyone can import it and pass it a ratings object (modeled off of the object stored at the ratings property in the reviewsMeta API return) to generate a rating object.
    - Added src/helpers directory to store helper modules

---
+++ client/src/components/overview/SocialMediaList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import SocialMediaButton from './SocialMediaButton.jsx';
+import facebookIcon from '../../assets/facebook-icon.svg';
+import twitterIcon from '../../assets/twitter-icon.svg';
+import pinterestIcon from '../../assets/pinterest-icon.svg';
+
+const SocialMedia = (props) => {
+  const sites = [
+    {
+      url: 'https://www.facebook.com',
+      icon: facebookIcon
+    },
+    {
+      url: 'https://www.twitter.com',
+      icon: twitterIcon
+    },
+    {
+      url: 'https://www.pinterest.com',
+      icon: pinterestIcon
+    }
+  ];
+
+  return (
+    <div>
+      {sites.map((site, index) => {
+        return <SocialMediaButton
+          url={site.url}
+          icon={site.icon}
+          key={index}
+        />;
+      })}
+    </div>
+  );
+};
+
+export default SocialMedia;
