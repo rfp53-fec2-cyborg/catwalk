9064c2af899d74fcecf5024c91b10c3cfca11b8c
godfreydoo <godfrey.doo@gmail.com>
2021-06-09 11:58:14 -0700

    Improve rating filter description

--- client/src/components/reviews/RatingFilterDesc.jsx
+++ client/src/components/reviews/RatingFilterDesc.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 const RatingFilterDesc = (props) => {
 
+  console.log(props.data);
+
   if (props.data.length !== 0) {
     return (
       <>
         <div>
-          Reviews are filtered by these ratings: {props.data.join(', ')}
+          Reviews are filtered by these ratings: {props.data.reduce((accumulator, value, index) => {
+            if (index === props.data.length - 1) {
+              return accumulator + value + ' stars ';
+            } else {
+              return accumulator + value + ' stars, ';
+            }
+          }, '')}
         </div>
         <span value={[]} onClick={() => props.setRating([])}>
           Remove all filters.
