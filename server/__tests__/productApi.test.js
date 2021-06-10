9fee7129a94f5b48fd3b275695e53e63c8c382fc
godfreydoo <godfrey.doo@gmail.com>
2021-06-10 16:07:45 -0700

    Remove API test and add env token for lighthouse actions

--- server/__tests__/productApi.test.js
+++ server/__tests__/productApi.test.js
@@ -1,11 +1,19 @@
 // jest.mock('./productApi');
-require('dotenv').config({path: 'server/.env'});
 
-const { getProducts, getProductsId, getProductsIdRelated, getProductsIdStyles } = require('../productApi.js');
 
-// The assertion for a promise must be returned.
-it('Returns an array of products when calling /GET products', async () => {
-  expect.assertions(1);
-  let data = await getProducts();
-  expect(data.length).toBeGreaterThanOrEqual(1);
-});
+
+/*
+  Prioritize mocking functions to test API results
+*/
+
+
+// require('dotenv').config({path: 'server/.env'});
+
+// const { getProducts, getProductsId, getProductsIdRelated, getProductsIdStyles } = require('../productApi.js');
+
+// // The assertion for a promise must be returned.
+// it('Returns an array of products when calling /GET products', async () => {
+//   expect.assertions(1);
+//   let data = await getProducts();
+//   expect(data.length).toBeGreaterThanOrEqual(1);
+// });
