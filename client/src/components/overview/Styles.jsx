f588a059c7587affb41484c469253f31c6185ee6
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-10 17:24:19 -0700

    Overview: added styles section (#24)
    - Added Styles component, which renders thumbnails of all styles
    - Created StylesRow component to help render rows of style thumbnails
    - Overview component now stores selectedStyle in state
    - Added click handler to detect clicks on styles and change selectedStyle
    - Minor change to props passed from Overview to Styles to simplify code
    - Added checkmark overlay for selected style thumbnail
    - Styles now shows name of currently selected style above the thumbnails
    - Minor refactor: saving full selected style to state instead of just the ID

---
+++ client/src/components/overview/Styles.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StylesRow from './StylesRow.jsx';
+
+const Styles = ({ styles, selectedStyle, handleStyleClick }) => {
+
+  const NUM_COLS = 4;
+
+  return (
+    <div>
+      <h4>
+        STYLE > <span style={{fontWeight: '500'}}>{selectedStyle.name}</span>
+      </h4>
+      <table>
+        <tbody>
+          {styles.map((style, index) => {
+            if (index % NUM_COLS === 0) {
+              return <StylesRow
+                key={`stylesRow_${index / 4}`}
+                styles={styles.slice(index, index + NUM_COLS)}
+                selectedStyle={selectedStyle}
+                handleStyleClick={handleStyleClick}
+              />;
+            }
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Styles;
