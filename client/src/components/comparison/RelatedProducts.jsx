57d34ec001c9d9ef7b77aa27c201e7aee3548725
Jacob Harrelson <random08@gmail.com>
2021-06-15 01:06:08 -0500

    removed erroneous logic and passsed/used live data

--- client/src/components/comparison/RelatedProducts.jsx
+++ client/src/components/comparison/RelatedProducts.jsx
@@ -1,18 +1,20 @@
 import React from 'React';
 import ProductCard from './ProductCard.jsx';
 
-const RelatedProducts = ({detaledRelatedProductsArr, mockStyles, mockProducts}) => {
-  // console.log(mockProducts);
+
+const RelatedProducts = ({detaledRelatedProductsArr, relatedProductsStylesArr}) => {
+  // console.log(detaledRelatedProductsArr);
+  // console.log(relatedProductsStylesArr);
 
   return (
     <div>
       <h3>Related Products</h3>
       <div id="productsContainer"style={{display: 'grid', gridTemplateColumns: '250px 250px 250px 250px 250px', gridTemplateRows: '375px, gap: 0px 0px', padding: '0px', margin: '20px 40px 20px', overflow: scroll}}>
-        {mockProducts && mockProducts.length > 0 && mockProducts.map((mockProduct, index) => {
+        {detaledRelatedProductsArr.map((detaledRelatedProduct, index) => {
           return <ProductCard
             key={index}
-            mockProduct={mockProduct}
-            mockStyles={mockStyles}
+            detaledRelatedProduct={detaledRelatedProduct}
+            relatedProductsStylesArr={relatedProductsStylesArr}
           />;
         })}
       </div>
