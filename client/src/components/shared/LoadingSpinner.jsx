3dbc209b58e7ac3a274e88c9f68580c3fef94a95
godfreydoo <godfrey.doo@gmail.com>
2021-06-14 19:39:33 -0700

    Add loading text to loading page

--- client/src/components/shared/LoadingSpinner.jsx
+++ client/src/components/shared/LoadingSpinner.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const LoadingSpinner = (props) => {
   return (
     <div className="loading-spinner-container">
+      Loading...
       <div className="loading-spinner">
         <div>
         </div>
