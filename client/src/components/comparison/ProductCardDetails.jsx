1e0cd8b9afe7ab360bdf20a14f5d21832c7820fd
Jacob Harrelson <random08@gmail.com>
2021-06-15 01:11:55 -0500

    using live server data, basic skeleton completed

---
+++ client/src/components/comparison/ProductCardDetails.jsx
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+
+const ProductCardDetails = ({detaledRelatedProduct, relatedProductsStylesArr}) => {
+
+  let currentId = detaledRelatedProduct.id;
+  console.log('styles',relatedProductsStylesArr);
+  console.log('detales',detaledRelatedProduct);
+
+
+
+  return (
+    <>
+      <div>{detaledRelatedProduct.category}</div>
+      <div>{detaledRelatedProduct.name}</div>
+      <div>{'$' + detaledRelatedProduct.default_price}</div>
+    </>
+  );
+};
+
+export default ProductCardDetails;
