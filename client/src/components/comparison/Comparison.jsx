c364e107b333bd4a9e9b25ae7003ac283a71574e
Jacob Harrelson <random08@gmail.com>
2021-06-15 01:04:08 -0500

    refactored to stateful class component, made 2 of 3 api calls and updated current state to reflect live data from app/api

--- client/src/components/comparison/Comparison.jsx
+++ client/src/components/comparison/Comparison.jsx
@@ -1,53 +1,95 @@
 import React, {useEffect, useState/*, useContext*/} from 'react';
 import RelatedProducts from './RelatedProducts.jsx';
 import UserOutfitItems from './UserOutfitItems.jsx';
-import {products} from '../../../mock-data/products.js';
-import {productStyles} from '../../../mock-data/productStyles.js';
+// import {products} from '../../../mock-data/products.js';
+// import {productStyles} from '../../../mock-data/productStyles.js';
 import Requester from '../../Requester.js';
-
-
-// console.log(products);
+import LoadingSpinner from '../../components/shared/LoadingSpinner.jsx';
 
 const rerequester = Requester();
-const Comparison = ({/*product,*/ relatedProducts, styles}) => {
-  let mockProducts = products;
-  let mockStyles = productStyles;
-  // console.log(mockStyles);
-  // const [data] = useState(mockProducts);
-  console.log(styles);
-  let detaledRelatedProductsArr = [];
-  // console.log(relatedProducts); // [17068, 17069, 17074, 17073]
-  let getTheDeets = ()=> {
-    return Promise.all(relatedProducts.map(relatedProduct => rerequester.getProduct(relatedProduct)))
-      .then((data) => {
-        // console.log('inside', data);
-        addOne(data);
-      })
-      .catch(console.log());
-  };
-  getTheDeets();
 
-  let addOne = (targets) => {
-    for (let i = 0; i < targets.length; i++) {
-      // console.log(targets[i]); // last place i can get to individual prods :'(
-      detaledRelatedProductsArr.push(targets[i]);
-    }
-    return detaledRelatedProductsArr;
-  };
-  // console.log('why cant i get the individual value?', detaledRelatedProductsArr); // maybe its an async thing? maybe i need to leverage the lifecycle and have it in state? maybe in index?
+class Comparison extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      relatedProducts: this.props.relatedProducts,
+      detaledRelatedProductsArr: [],
+      relatedProductsStylesArr: [],
+      outfitList: [],
+      activeIndex: 0,
+      styles: this.props.styles
+    };
+    // this.fetchAndStore = this.props.fetchAndStore.bind(this);
+  }
 
-  // need several functions here
+
+
   // function that will handle product click
+
   // function that will handle the compare Modal
+
   // function that will handle adding an item to the outfit list
+
+  // handleAddOutfitItem = (product, style) => {
+  //   if (!outfitList.some(obj => obj.id === product.id)) {
+  //     this.setState({
+  //       outfitList: [...outfitList, product]});
+  //   }
+  // };
+
   // function that will handle removing an item form the outfit list
 
-  return (
-    <div style={{display: 'grid', gridTemplateColumns: '=400px', gridTemplateRows: '400px', gap: '0px 0px'}}>
-      <RelatedProducts detaledRelatedProductsArr={detaledRelatedProductsArr} mockProducts={mockProducts} mockStyles={mockStyles}/>
-      {/* <UserOutfitItems productStyles={productStyles}/> */}
-    </div>
-  );
-};
+  // get and store the product obj from each product in the relatedProducts array
+  getTheDeets = ()=> {
+    return Promise.all(this.state.relatedProducts.map(relatedProduct => rerequester.getProduct(relatedProduct)))
+      .then((data) => {
+        // console.log('this is working', data);
+        this.setState({
+          detaledRelatedProductsArr: data
+        });
+      })
+      .then(this.getTheStyles())
+      .catch(console.log('err?'));
+  };
+
+  getTheStyles = ()=> {
+    return Promise.all(this.state.relatedProducts.map(relatedProduct => rerequester.getProductStyles(`${relatedProduct}`)))
+      .then((data) => {
+        // console.log('hit', data);
+        this.setState({
+          relatedProductsStylesArr: data
+        });
+      })
+      // .then(this.setState({
+      //   isLoaded: true
+      // }))
+      .catch(console.log('err??'));
+  };
+
+  componentDidMount() {
+    this.getTheDeets();
+  }
+
+  render() {
+    // console.log(this.state.styles);
+    // console.log('loaded detaledRelatedProductsArr', this.state.detaledRelatedProductsArr);
+    // console.log('relstyles', this.state.relatedProductsStylesArr);
+
+
+    return (
+      <div>
+        { this.state.relatedProductsStylesArr.length ?
+          <div style={{display: 'grid', gridTemplateColumns: '=400px', gridTemplateRows: '400px', gap: '0px 0px'}}>
+            <RelatedProducts detaledRelatedProductsArr={this.state.detaledRelatedProductsArr} relatedProductsStylesArr={this.state.relatedProductsStylesArr}/>
+            {/* <UserOutfitItems productStyles={productStyles}/> */}
+          </div> :
+          <>
+            <LoadingSpinner />
+          </>
+        }
+      </div>
+    );
+  }
+}
 
 export default Comparison;
