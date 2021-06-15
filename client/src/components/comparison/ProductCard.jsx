0b2c2edb9929f8893c14c59b51c9c0a725d83021
Jacob Harrelson <random08@gmail.com>
2021-06-15 01:08:40 -0500

    Created function to find and use correct image, seperated out other details into another component.  Using live data o Please enter the commit message for yoon to iines starting

--- client/src/components/comparison/ProductCard.jsx
+++ client/src/components/comparison/ProductCard.jsx
@@ -1,10 +1,30 @@
 import React, {useEffect, useState} from 'react';
+import ProductCardDetails from './ProductCardDetails.jsx';
 /* import star rating? */
 
-const ProductCard = ({mockProduct, mockStyles}) => {
-  console.log(mockStyles.results[0].photos[0].thumbnail_url);
+const ProductCard = ({detaledRelatedProduct, relatedProductsStylesArr}) => {
+  // console.log(detaledRelatedProduct.id);
+  let currentId = detaledRelatedProduct.id;
+  // console.log(relatedProductsStylesArr);
 
   // function will need to filter the correct photo here later
+  const getTheCorrectPic = (relatedProductsStylesArr, currentId) => {
+    let possiblePicUrls = [];
+    for (let i = 0; i < relatedProductsStylesArr.length; i++) {
+      let current = relatedProductsStylesArr[i];
+      if (Number(current.product_id) === currentId) {
+        possiblePicUrls.push(current);
+      }
+    }
+    // console.log(possiblePicUrls);
+    if (possiblePicUrls[0]) {
+      if (possiblePicUrls[0].results[0].photos[0].thumbnail_url) {
+        let urlPic = possiblePicUrls[0].results[0].photos[0].thumbnail_url;
+        return urlPic;
+      }
+    }
+  };
+  // const image = relatedProductsStylesArr[1].results[0].photos[0].thumbnail_url;
 
   // function to have compare modal button/icon to go here
 
@@ -12,11 +32,9 @@ const ProductCard = ({mockProduct, mockStyles}) => {
     <div style={{borderStyle: 'solid'}}/*id="productCard"style={{display: 'grid', gridTemplateColumns: '1fr', gridTemplateRows: '60% 40%', gap: '0px 0px', borderStyle: 'solid', borderWidth: '2px', margin: '5px'}}*/>
       {/* button/icon to bring up compare modal will go here */}
       <div>
-        <img src={mockStyles.results[0].photos[0].thumbnail_url}/>
+        <img src={getTheCorrectPic(relatedProductsStylesArr, currentId)} alt='Image Not Available'/>
       </div>
-      <div>{mockProduct.category}</div>
-      <div>{mockProduct.name + ', ' + mockProduct.slogan}</div>
-      <div>{'$' + mockProduct.default_price}</div>
+      <ProductCardDetails detaledRelatedProduct={detaledRelatedProduct} relatedProductsStylesArr={relatedProductsStylesArr}/>
       <div>Star Rating</div>
     </div>
   );
