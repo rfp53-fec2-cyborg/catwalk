07333f0a03b19e3160071d0ebfa98604bca65c48
godfreydoo <godfrey.doo@gmail.com>
2021-06-16 00:13:50 -0700

    Commenting our console.log statements from previous merge

--- client/src/components/comparison/ProductCardDetails.jsx
+++ client/src/components/comparison/ProductCardDetails.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 const ProductCardDetails = ({detailedRelatedProduct, relatedProductsStylesArr}) => {
-  console.log(detailedRelatedProduct);
+  // console.log(detailedRelatedProduct);
   return (
     <>
       <div>{detailedRelatedProduct.category}</div>
