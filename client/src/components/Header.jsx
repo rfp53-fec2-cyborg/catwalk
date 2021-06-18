6d91c5fe875382c2c5af29fb0f3f9f288470b5a9
chefferson <49037676+chefferson@users.noreply.github.com>
2021-06-17 17:13:53 -0700

    - Cleaned up some code in Header component - Added some color hexes that were missing from dark mode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
           type="radio"
           name={value}
           value={value}
-          checked={theme === `${value}`}
+          checked={theme === value}
           onChange={handleChange}
         />
         {capitalize(value)}
